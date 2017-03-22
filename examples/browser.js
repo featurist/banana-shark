6d7dd45e810b94ced49f973f420a3d2d50a5566c
Josh Chisholm <joshuachisholm@gmail.com>
2017-03-22 11:09:28 +0000

    Clarify browser example

--- examples/browser.js
+++ examples/browser.js
@@ -1,5 +1,3 @@
-const FinishedPromise = require('finished-promise')
-
 module.exports = describe => {
 
   describe(
@@ -25,6 +23,33 @@ module.exports = describe => {
   )
 }
 
+// a utility function to create an element and mount an app with an "automator"
+function weatherApp () {
+  const element = document.createElement('div')
+  element.className = 'weather-app'
+  document.body.appendChild(element)
+  return new WeatherAppAutomator({ element })
+}
+
+// the class under test represents a web app
+class WeatherApp {
+  constructor ({ element, weatherService }) {
+    this.element = element
+    this.weatherService = weatherService
+  }
+
+  showWeatherForecast () {
+    return this.weatherService.forecast()
+      .then(outlook => {
+        this.element.innerText = outlook
+      })
+      .catch(() => {
+        this.element.innerText = 'Please try again later'
+      })
+  }
+}
+
+// builders are useful for building up nested contexts
 class Builder {
   constructor (options) {
     this.options = options
@@ -36,6 +61,9 @@ class Builder {
   }
 }
 
+// automating a web app in a specific context means first building up the
+// context (some service is online/offline) then interacting with the app
+// in different ways (visit the weather forecast)
 class WeatherAppAutomator extends Builder {
   constructor (options) {
     super()
@@ -56,22 +84,11 @@ class WeatherAppAutomator extends Builder {
   }
 }
 
-class WeatherApp {
-  constructor ({ element, weatherService }) {
-    this.element = element
-    this.weatherService = weatherService
-  }
+// Use stubs to represent the interactions with external dependencies. Using
+// FinishedPromise instead of Promise means we can keep our tests synchronous,
+// while our application code is asynchronous
 
-  showWeatherForecast () {
-    return this.weatherService.forecast()
-      .then(outlook => {
-        this.element.innerText = outlook
-      })
-      .catch(() => {
-        this.element.innerText = 'Please try again later'
-      })
-  }
-}
+const FinishedPromise = require('finished-promise')
 
 class OfflineWeatherService {
   forecast () {
@@ -83,11 +100,4 @@ class StubWeatherService {
   forecast () {
     return FinishedPromise.resolve('The weather forecast')
   }
-}
-
-function weatherApp () {
-  const element = document.createElement('div')
-  element.className = 'weather-app'
-  document.body.appendChild(element)
-  return new WeatherAppAutomator({ element })
 }
